e316dabe3910f8745697b1ffc3d530a312cfadc0
Jixian Wang <jixian@pickatale.com>
2014-03-10 19:06:50 +0800

    update

--- templates/module/backend/model/model-template.js
+++ templates/module/backend/model/model-template.js
@@ -6,5 +6,11 @@ module.exports = function(app)
         collection: '@@MODEL_COLLECTION@@'
     });
 
+    //sample for add static method
+    /// schema.statics.methodName = function(parameter1,parameter2){}
+
+    //sample for add dynamic method
+	/// schema.methods.methodName = function(parameter1,parameter2){}
+
     return app.openbiz.db.model('@@APP_NAME@@.@@MODULE_NAME@@.@@MODEL_NAME@@', schema);
 }
