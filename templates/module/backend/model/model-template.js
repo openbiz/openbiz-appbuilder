532ca449787d68af042ea68a6afbab5a51cc0b7e
Jixian Wang <jixian@pickatale.com>
2014-03-20 13:03:35 +0800

    appbuilder 生成后台JSON文件可以支持metadata分离

--- templates/module/backend/model/model-template.js
+++ templates/module/backend/model/model-template.js
@@ -2,7 +2,11 @@
 module.exports = function(app)
 {
     var mongoose = app.openbiz.mongoose;
-    var schema = new mongoose.Schema({{MODEL_SCHEMA}},{
+    var schema = new mongoose.Schema(function(){
+        var schema = require(__filename.replace(/\.js$/i,'.json'));
+        //advanced parse config json can be done here
+        return schema;
+    }(),{
         collection: '{{MODEL_COLLECTION}}'
     });
     {% for(var i=0;i<MODEL_ACTION_STATICS.length;i++){ var ACTION = MODEL_ACTION_STATICS[i]; %}
