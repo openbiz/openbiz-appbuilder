3d74be10ace7fc60347bb1224afd9e37776c091f
Jixian Wang <jixian@pickatale.com>
2014-03-25 18:19:35 +0800

    bug fix

--- templates/module/frontend/router/router-template.js
+++ templates/module/frontend/router/router-template.js
@@ -3,7 +3,7 @@ define(function(){
     return openbiz.Router.extend({		
         app: openbiz.apps.{{APP_NAME}}?openbiz.apps.{{APP_NAME}}:'{{APP_NAME}}',
         routes:{
-          {% for(var i=0;i<ROUTES.length;i++){ var route = ROUTES[i]; %}"!/backend{{route.URL}}"    : "{{route.FUNCTION}}"{% if(i<(ROUTES.length-1)){ %},{% } %}
+          {% for(var i=0;i<ROUTES.length;i++){ var route = ROUTES[i]; %}"!/backend{{APP_URL}}{{route.URL}}"    : "{{route.FUNCTION}}"{% if(i<(ROUTES.length-1)){ %},{% } %}
           {% } %}
         },
         initialize:function(){          
